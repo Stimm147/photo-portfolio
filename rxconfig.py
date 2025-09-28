b8b1658645b7d34742d867bfc7ba16b361ee7d31
stimm147 <michalczewski0@gmail.com>
2025-09-29 00:13:33 +0200

    rx config modified

--- rxconfig.py
+++ rxconfig.py
@@ -3,11 +3,18 @@ import os
 
 api_url = os.getenv("API_URL", "http://127.0.0.1:8000")
 
+
 config = rx.Config(
-    app_name="main_page",
+    app_name="portfolio",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
     ],
     api_url=api_url,
+    cors_allowed_origins=[
+        "http://localhost:3000",
+        "https://photo-portfolio-production.up.railway.app",
+    ],
+    compile=True,
+    telemetry_enabled=False,
 )
