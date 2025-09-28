e79aa3481c20d9625c1b14b4113e495795d7138c
stimm147 <michalczewski0@gmail.com>
2025-09-29 01:33:13 +0200

    prod env added

--- rxconfig.py
+++ rxconfig.py
@@ -1,8 +1,12 @@
 import reflex as rx
 import os
+import gc
 
 api_url = os.getenv("API_URL", "http://127.0.0.1:8000")
 
+if os.getenv("ENV") == "prod":
+    # Włącz aggressive garbage collection
+    gc.set_threshold(100, 10, 10)
 
 config = rx.Config(
     app_name="reflex_example",
