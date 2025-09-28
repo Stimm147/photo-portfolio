3178b301eb7aab69c5ae22f2aec9469e7d40c177
stimm147 <michalczewski0@gmail.com>
2025-09-29 01:17:23 +0200

    flask test

--- reflex_example/reflex_example.py
+++ reflex_example/reflex_example.py
@@ -1,5 +1,6 @@
 import reflex as rx
 from rxconfig import config
+from reflex_example.api_connector import classifier_component
 
 
 class State(rx.State):
@@ -10,25 +11,26 @@ class State(rx.State):
 
 def index() -> rx.Component:
     """The main page of the app."""
-    return rx.container(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
-            ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
-            spacing="5",
-            justify="center",
-            min_height="83vh",
-        ),
-    )
+    return classifier_component
+    # return rx.container(
+    #     rx.color_mode.button(position="top-right"),
+    #     rx.vstack(
+    #         rx.heading("Welcome to Reflex!", size="9"),
+    #         rx.text(
+    #             "Get started by editing ",
+    #             rx.code(f"{config.app_name}/{config.app_name}.py"),
+    #             size="5",
+    #         ),
+    #         rx.link(
+    #             rx.button("Check out our docs!"),
+    #             href="https://reflex.dev/docs/getting-started/introduction/",
+    #             is_external=True,
+    #         ),
+    #         spacing="5",
+    #         justify="center",
+    #         min_height="83vh",
+    #     ),
+    # )
 
 
 app = rx.App(
